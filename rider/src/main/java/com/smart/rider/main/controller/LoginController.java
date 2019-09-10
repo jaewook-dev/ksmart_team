024a2e44fc1c0ce6361c780613d2d190429e43e3
Administrator <Administrator@smart000>
2019-09-10 13:58:32 +0900

    재욱, 추가 : member 테이블에서 누락된 컬럼
    MemberDTO에 member 테이블에서 누락된 매장코드 추가

--- rider/src/main/java/com/smart/rider/main/controller/LoginController.java
+++ rider/src/main/java/com/smart/rider/main/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.smart.rider.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -9,5 +12,13 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login() {
 		return "/login/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam(value = "membeId") String memberId
+						,@RequestParam(value = "memberPw") String memberPw
+						,Model model) {
+		System.out.println(memberId + " <-- memberId login");
+		return "redirect:/";
 	}
 }
