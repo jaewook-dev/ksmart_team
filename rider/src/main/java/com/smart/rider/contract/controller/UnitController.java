4b6eef10bfafdfcd48b298bea21d7780226466cf
Administrator <Administrator@smart20114>
2019-09-10 17:20:11 +0900

    석호 생성 :  계약단가표 생성,조회
    html파일만 생성
    추가할점 : CRUD 생성 및 폼 보완하기

--- rider/src/main/java/com/smart/rider/contract/controller/UnitController.java
+++ rider/src/main/java/com/smart/rider/contract/controller/UnitController.java
@@ -2,6 +2,7 @@ package com.smart.rider.contract.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.smart.rider.contract.service.UnitService;
 
@@ -9,4 +10,7 @@ import com.smart.rider.contract.service.UnitService;
 public class UnitController {
 
 	@Autowired UnitService unitService;
+	
+	
+	
 }
