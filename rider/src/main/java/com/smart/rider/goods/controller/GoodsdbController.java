6d240dba7cd9222d84c158161833cf927d0d7fdd
smart-202-21 <smart-202-21@smart-202-21-PC>
2019-09-11 09:42:48 +0900

    영성 : 0911 변수 카멜식으로변환

--- rider/src/main/java/com/smart/rider/goods/controller/GoodsdbController.java
+++ rider/src/main/java/com/smart/rider/goods/controller/GoodsdbController.java
@@ -16,37 +16,38 @@ public class GoodsdbController {
 	@Autowired
 	public GoodsdbService goodsdbservice;
 	
-	//상품db등록 페이지이동
+	//01상품db등록 페이지이동
 	@GetMapping("/goodsdb")
 	public String goods() {
 		return "/goods/goodsdb";
 	}
 	
-	@GetMapping("/goodsdbInsert")
+	@GetMapping("/goodsDbInsert")
 	public String goodsdbInsert() {
-		return "/goods/goodsdbInsert";
+		return "/goods/goodsDbInsert";
 	}
 	
-	/*
+	/*02
 	 * 상품DB등록 확인메서드(처음단계)
 	 * 문영성
-	 * @PostMapping("/goodsdbInsert") public String goodsdbInsert(GoodsdbDTO
+	 * @PostMapping("/goodsDbInsert") public String goodsdbInsert(GoodsdbDTO
 	 * goodsdbdto,Model model) {
 	 * //System.out.println(goodsdbdto+"<<<<<<<<<<넘어오는값 확인하기");
 	 * model.addAttribute("goodsdbdto",goodsdbservice.goodsdbInsert(goodsdbdto));
-	 * return "/goods/goodsdbInsert";
+	 * return "/goods/goodsDbInsert";
 	 * 
 	 * }
 	 */
 	
 	
 	
-	//상품db 세션아이디가져와서 코드자동증가, db에값들어가는지확인.
+	//
+	//03상품db 세션아이디가져와서 코드자동증가, db에값들어가는지확인.
 	//19-09-10 문영성
-	@PostMapping("/goodsdbInsert")
-		public String goodsdbInsert(GoodsdbDTO goodsdbdto, HttpSession session) {
+	@PostMapping("/goodsDbInsert")
+		public String goodsDbInsert(GoodsdbDTO goodsdbdto, HttpSession session) {
 	
-			goodsdbservice.goodsdbInsert(goodsdbdto, session);
+			goodsdbservice.goodsDbInsert(goodsdbdto, session);
 		return "redirect:/goodsdb";			
 		}
 	
