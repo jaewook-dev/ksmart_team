e31a727501c7ab6a7b010f21a5d6e11df7a78542
Administrator <Administrator@smart20114>
2019-09-11 10:46:25 +0900

    석호 추가:메인관리
    main에 계정단가표DB(가짜)세팅 완료

--- rider/src/main/java/com/smart/rider/main/service/MainService.java
+++ rider/src/main/java/com/smart/rider/main/service/MainService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.smart.rider.contract.dto.UnitDTO;
 import com.smart.rider.main.mapper.MainMapper;
 import com.smart.rider.subject.dto.SubjectDTO;
 
@@ -19,10 +20,15 @@ public class MainService {
 	// 재욱, 초기 index 화면 계정과목 DB 연결 확인용 추후 삭제
 	public Map<String, Object> subjectList(){
 		Map<String, Object> map = new HashMap<String, Object>();
+		
 		List<SubjectDTO> list = mainMapper.subjectList();
 		//System.out.println(list + " <-- list subjectList MainService.java");
+		List<UnitDTO> Ulist = mainMapper.UnitList();
+		//System.out.println(list + " <-- Ulist UnitList MainService.java");
 		map.put("subjectList", list);
-		// System.out.println(map.get("subjectList") + " <-- map.put 확인 MainService.java"); ;
+		// System.out.println(map.get("subjectList") + " <-- map.put 확인 MainService.java");
+		map.put("unitList", Ulist);
+		
 		return map;
 	}
 }
