c97996476bec70ad17ce9efc2e9b4282fa1c55e1
Administrator <Administrator@smart20114>
2019-09-11 14:32:17 +0900

    석호 추가 : 계약관리
    계약관리에 계약단가표 추가 준비 및 계약단가표 조회, 코드 정리

--- rider/src/main/java/com/smart/rider/contract/mapper/UnitMapper.java
+++ rider/src/main/java/com/smart/rider/contract/mapper/UnitMapper.java
@@ -1,9 +1,14 @@
 package com.smart.rider.contract.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.smart.rider.contract.dto.UnitDTO;
 
 @Mapper
 public interface UnitMapper {
-
+	
+	public List<UnitDTO> UnitList();
 	
 }
