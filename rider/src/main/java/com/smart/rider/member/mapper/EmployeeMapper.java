f83a67f22f2afe48c648d595fa291907746cc44b
Familly_P <Familly_P@DESKTOP-VBHHC7E>
2019-09-15 16:26:49 +0900

    건영, 추가 : employee 리스트와 등록
    list와 insert mapper/service/controller

--- rider/src/main/java/com/smart/rider/member/mapper/EmployeeMapper.java
+++ rider/src/main/java/com/smart/rider/member/mapper/EmployeeMapper.java
@@ -1,11 +1,15 @@
 package com.smart.rider.member.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.smart.rider.member.dto.MemberDTO;
 
 @Mapper
 public interface EmployeeMapper {
-
-	public int EmployeeInsert(MemberDTO memberdto);
+	//19.09.15작성
+	public int employeeInsert(MemberDTO memberdto);
+	
+	public List<MemberDTO> employeeList();
 }
--- rider/src/main/java/com/smart/rider/member/service/EmployeeService.java
+++ rider/src/main/java/com/smart/rider/member/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.smart.rider.member.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.smart.rider.member.dto.MemberDTO;
 import com.smart.rider.member.mapper.EmployeeMapper;
 
 @Service
@@ -10,4 +13,15 @@ public class EmployeeService {
 	
 	@Autowired
 	private EmployeeMapper employeeMapper;
+	//19.09.15작성
+	public int employeeInsert(MemberDTO memberdto) {
+		
+		return employeeMapper.employeeInsert(memberdto);
+	}
+	
+	public List<MemberDTO> employeeList() {
+		
+		List<MemberDTO> list = employeeMapper.employeeList();
+		return list;
+	}
 }
