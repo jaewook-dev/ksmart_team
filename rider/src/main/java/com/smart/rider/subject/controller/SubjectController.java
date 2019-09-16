a3bed95ee30f2453e7a5c961e6aac2adac20a8d1
Administrator <Administrator@smart000>
2019-09-16 15:12:27 +0900

    재욱, 추가 : 문자열, 숫자 판별 기능
    StringCheck.java 문자열, 숫자 판별 기능 추가
    계정과목 등록시 코드부분에 문자열 입력시 생기는 오류를 판별을 통해 해결

--- rider/src/main/java/com/smart/rider/subject/controller/SubjectController.java
+++ rider/src/main/java/com/smart/rider/subject/controller/SubjectController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.rider.main.controller.StringCheck;
 import com.smart.rider.subject.dto.SubjectDTO;
 import com.smart.rider.subject.service.SubjectService;
 
@@ -36,12 +37,21 @@ public class SubjectController {
 	
 	// 계정과목 등록 
 	@PostMapping("/subjectInsert")
-	public String subjectInsert(SubjectDTO subjectDTO, HttpSession session) {
+	public String subjectInsert(SubjectDTO subjectDTO, HttpSession session, Model model) {
 		//System.out.println(subjectDTO.getSubjectNumber() + " <-- subjectNumber subjectInsert SubjectController.java");
 		//System.out.println(subjectDTO.getSubjectName() + " <-- subjectName subjectInsert SubjectController.java");
 		String memberId = (String)session.getAttribute("SID");
-		subjectService.subjectInsert(subjectDTO, memberId);
-		return "redirect:/subjectList";
+		
+		// 문자열, 숫자 판별
+		if(StringCheck.isNumeric(subjectDTO.getSubjectNumber()) == false) {
+			//System.out.println("미스 매칭 subjectInsert SubjectController.java");
+			model.addAttribute("alert", "계정과목코드는 숫자만 입력하세요");
+			return "subject/subjectInsert";
+		} else {
+			subjectService.subjectInsert(subjectDTO, memberId);
+			return "redirect:/subjectList";
+		}
+		
 	}
 	
 	// 계정과목 삭제
