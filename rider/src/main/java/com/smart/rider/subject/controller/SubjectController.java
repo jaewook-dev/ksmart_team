9aa39b5ff55c7c585d476ead4faa3c215a8e7553
Administrator <Administrator@smart000>
2019-09-16 11:31:26 +0900

    재욱, 생성 : 계정과목 등록 폼
    subjectInsert.html 계정과목 등록 폼 생성
    subjectList.html 계정과목 등록 버튼 클릭시 이동

--- rider/src/main/java/com/smart/rider/subject/controller/SubjectController.java
+++ rider/src/main/java/com/smart/rider/subject/controller/SubjectController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.smart.rider.subject.dto.SubjectDTO;
 import com.smart.rider.subject.service.SubjectService;
@@ -16,11 +18,27 @@ public class SubjectController {
 	@Autowired
 	private SubjectService subjectService;
 	
+	// 계정과목 리스트 화면
 	@GetMapping("/subject")
 	public String subject(Model model) {
 		List<SubjectDTO> list = subjectService.subjectList();
 		model.addAttribute("subjectList", list);
 		return "subject/subjectList";
+	}
+	
+	// 계정과목 등록 화면 이동 
+	@GetMapping("/subjectInsert")
+	public String subjectInsert() {
+		return "subject/subjectInsert";
+	}
+	
+	// 계정과목 등록 
+	@PostMapping("/subjectInsert")
+	public String subjectInsert( @RequestParam(value = "subjectNumber") String subjectNumber
+								,@RequestParam(value = "subjectName") String subjectName) {
+		//System.out.println(subjectNumber + " <-- subjectNumber subjectInsert SubjectController.java");
+		//System.out.println(subjectName + " <-- subjectName subjectInsert SubjectController.java");
+		return "subject/subjectInsert";
 	}
 
 }
