c86d4e09ed3bebb3cc76997ae8ab3b1cdce9faae
Administrator <Administrator@smart20114>
2019-09-23 16:57:11 +0900

    계약금 관리: 추가
    header부분에 경로 연결 및
    
    inner join으로 조회해서 명단 화면에 뿌려줄 예정

--- rider/src/main/java/com/smart/rider/contract/controller/ManagementController.java
+++ rider/src/main/java/com/smart/rider/contract/controller/ManagementController.java
@@ -2,6 +2,8 @@ package com.smart.rider.contract.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.smart.rider.contract.service.ManagementService;
 
@@ -10,4 +12,10 @@ public class ManagementController {
 
 	@Autowired
 	private ManagementService managementService;
+	
+	@GetMapping("/managementList")
+	public String managementList(Model model) {
+		
+		return "management/managementList";
+	}
 }
--- rider/src/main/java/com/smart/rider/contract/dto/ManagementDTO.java
+++ rider/src/main/java/com/smart/rider/contract/dto/ManagementDTO.java
@@ -2,5 +2,94 @@ package com.smart.rider.contract.dto;
 
 //계약금관리 DTO
 public class ManagementDTO {
+	
+	//명단조회할 때 필요한 DTO
+	private String contractMethod;
+	private int contractPay;
+	//생성시에 필요한 DTO
+	private String memberId;
+	private String contractManagementCode;
+	private String contractCode;
+	private int contractManagementAmout;
+	private int contractManagementUnpaid;
+	private String contractManagementState;
+	private String contractManagementContents;
+	private String contractManagementDate;
+	
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getContractMethod() {
+		return contractMethod;
+	}
+	public void setContractMethod(String contractMethod) {
+		this.contractMethod = contractMethod;
+	}
+	public int getContractPay() {
+		return contractPay;
+	}
+	public void setContractPay(int contractPay) {
+		this.contractPay = contractPay;
+	}
+	public String getContractManagementCode() {
+		return contractManagementCode;
+	}
+	public void setContractManagementCode(String contractManagementCode) {
+		this.contractManagementCode = contractManagementCode;
+	}
+	public String getContractCode() {
+		return contractCode;
+	}
+	public void setContractCode(String contractCode) {
+		this.contractCode = contractCode;
+	}
+	public int getContractManagementAmout() {
+		return contractManagementAmout;
+	}
+	public void setContractManagementAmout(int contractManagementAmout) {
+		this.contractManagementAmout = contractManagementAmout;
+	}
+	public int getContractManagementUnpaid() {
+		return contractManagementUnpaid;
+	}
+	public void setContractManagementUnpaid(int contractManagementUnpaid) {
+		this.contractManagementUnpaid = contractManagementUnpaid;
+	}
+	public String getContractManagementState() {
+		return contractManagementState;
+	}
+	public void setContractManagementState(String contractManagementState) {
+		this.contractManagementState = contractManagementState;
+	}
+	public String getContractManagementContents() {
+		return contractManagementContents;
+	}
+	public void setContractManagementContents(String contractManagementContents) {
+		this.contractManagementContents = contractManagementContents;
+	}
+	public String getContractManagementDate() {
+		return contractManagementDate;
+	}
+	public void setContractManagementDate(String contractManagementDate) {
+		this.contractManagementDate = contractManagementDate;
+	}
+	@Override
+	public String toString() {
+		return "ManagementDTO [memberId=" + memberId + ", contractMethod=" + contractMethod + ", contractPay="
+				+ contractPay + ", contractManagementCode=" + contractManagementCode + ", contractCode=" + contractCode
+				+ ", contractManagementAmout=" + contractManagementAmout + ", contractManagementUnpaid="
+				+ contractManagementUnpaid + ", contractManagementState=" + contractManagementState
+				+ ", contractManagementContents=" + contractManagementContents + ", contractManagementDate="
+				+ contractManagementDate + "]";
+	}
 
+	
+	
+	
 }
+
+
