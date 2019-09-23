07d36cc719768250db2b6e14d6d8daae552782ec
Administrator <Administrator@MSDN-SPECIAL>
2019-09-23 12:18:59 +0900

    건영, 추가 : 업데이트 form 수정
    리스트에서 대여고객정보수정 화면으로 이동까지 추가

--- rider/src/main/java/com/smart/rider/customer/controller/CustomerController.java
+++ rider/src/main/java/com/smart/rider/customer/controller/CustomerController.java
@@ -1,9 +1,14 @@
 package com.smart.rider.customer.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.rider.customer.dto.CustomerDTO;
 import com.smart.rider.customer.service.CustomerService;
 
 @Controller
@@ -11,14 +16,22 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
+	
 	//19.09.20작성
-	@GetMapping("/customerList")
-	public String customerList() {
-		return "customer/customerList";
-	}
 	@GetMapping("/customerInsert")
 	public String customerInsert() {
 		return "customer/customerInsert";
 	}
-	
+	//19.09.23작성
+	@GetMapping("/customerList")
+	public String customerList(Model model) {
+		List<CustomerDTO> list = customerService.customerList();
+		model.addAttribute("custoemrList", list);
+		return "customer/customerList";
+	}
+	@GetMapping("/getCustomerList")
+	public String getCustomerList(@RequestParam(value="rentalCustomerCode") String rentalCustomerCode, Model model) {
+		model.addAttribute("customerList", customerService.getCustomerList(rentalCustomerCode));
+		return "customer/customerUpdate";
+	}
 }
