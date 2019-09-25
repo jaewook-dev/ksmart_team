92bc12aa573e74b03c383dc53bdcef15584277a3
smart-202-21 <smart-202-21@smart-202-21-PC>
2019-09-25 12:12:35 +0900

    영성: 대여상품 등록처리완료 리스트 보기 완료
    대여상품등록처리 완료
    
    리스트 보기 완료
    
    상세보기완료

---
+++ rider/src/main/java/com/smart/rider/goods/mapper/GoodsRentalMapper.java
@@ -0,0 +1,21 @@
+package com.smart.rider.goods.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.smart.rider.goods.dto.GoodsHapDTO;
+import com.smart.rider.goods.dto.GoodsRentalDTO;
+@Mapper
+public interface GoodsRentalMapper {
+	
+	//코드자동증가
+	public String goodsRentalCodeCount();
+	//대여상품 상세조회 insert
+	public GoodsHapDTO getGoodsRentalList(String goodsRentalCode);
+	//대여상품등록 insert
+	public int goodsRentalInsert(GoodsRentalDTO goodsRentalDto);
+	//대여상품 리스트
+	public List<GoodsHapDTO> goodsRentalList();
+	
+}
