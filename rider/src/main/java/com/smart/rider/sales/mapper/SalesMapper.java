1f27f1358249f36ef4dcfa5cea0fee647b0bb54b
smart-202-21 <smart-202-21@smart-202-21-PC>
2019-09-26 16:04:54 +0900

    영성 : 매출 등록시작

--- rider/src/main/java/com/smart/rider/sales/mapper/SalesMapper.java
+++ rider/src/main/java/com/smart/rider/sales/mapper/SalesMapper.java
@@ -1,5 +1,13 @@
 package com.smart.rider.sales.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.smart.rider.goods.dto.GoodsHapDTO;
+
+@Mapper
 public interface SalesMapper {
+	public List<GoodsHapDTO> salesList();
 
 }
--- rider/src/main/java/com/smart/rider/sales/service/SalesService.java
+++ rider/src/main/java/com/smart/rider/sales/service/SalesService.java
@@ -1,12 +1,23 @@
 package com.smart.rider.sales.service;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.smart.rider.goods.dto.GoodsHapDTO;
+import com.smart.rider.sales.mapper.SalesMapper;
 
 
 @Service
 @Transactional
 public class SalesService {
-
+	@Autowired
+	private SalesMapper salesMapper;
+	
+	public List<GoodsHapDTO> salesList(){
+		return salesMapper.salesList();
+	}
 }
