fc3ac499ff2cd4dfca26ec1e3a1702819f9e7a7f
Administrator <Administrator@smart000>
2019-09-26 14:08:16 +0900

    재욱, 수정 : 차트 캡슐화
    재사용 용이를 위한 월별 지출 금액 차트 캡슐화

--- rider/src/main/java/com/smart/rider/main/controller/MainController.java
+++ rider/src/main/java/com/smart/rider/main/controller/MainController.java
@@ -1,37 +1,15 @@
 package com.smart.rider.main.controller;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.smart.rider.main.service.MainService;
 
 @Controller
 public class MainController {
-	
-	@Autowired
-	private MainService mainService;
-	
+		
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index() {
 		
-		// 재욱, 초기 index 화면 계정과목 DB 연결 확인용 추후 삭제
-		Map<String, Object> map = mainService.subjectList();
-		
-		
-		//System.out.println(map.get("subjectList") + " <-- subjectList index MainController.java");
-		model.addAttribute("subjectList", map.get("subjectList"));
-		model.addAttribute("unitList", map.get("unitList"));
-
-		//영성 index화면 상품db연결 확인 (삭제용)
-		//System.out.println(map.get("goodsDbList")+"<-------------goodsDbList확인----MainController.java");
-		model.addAttribute("goodsDbList",map.get("goodsDbList"));
-
-		model.addAttribute("memberList", map.get("memberList"));		
-
 		return "index";
 	}
 	
--- rider/src/main/java/com/smart/rider/spend/controller/UtilityController.java
+++ rider/src/main/java/com/smart/rider/spend/controller/UtilityController.java
@@ -1,5 +1,6 @@
 package com.smart.rider.spend.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.rider.main.service.MainService;
 import com.smart.rider.spend.dto.JoinUtilityDTO;
 import com.smart.rider.spend.dto.UtilityDTO;
 import com.smart.rider.spend.dto.UtilityPay;
@@ -24,6 +26,9 @@ public class UtilityController {
 	@Autowired
 	private UtilityService utilityService;
 	
+	@Autowired
+	private MainService mainService;
+	
 	// 지출_공과금 등록내역 상세보기
 	@GetMapping("/spendUtilityDetails")
 	public String spendUtilityDetails(String spendUtilityCode, Model model) {
@@ -35,7 +40,7 @@ public class UtilityController {
 		return "spend/spendUtilityDetails";
 	}
 	
-	// 지출_공과금 검색
+	/*** 190925 재욱, 지출_공과금 검색 ***/
 	@PostMapping("/spendUtilityList")
 	public String utilityList(@RequestParam(value = "utilityKey") String utilityKey
 							, @RequestParam(value = "utilityValue") String utilityValue
@@ -53,7 +58,8 @@ public class UtilityController {
 		
 		String contractShopCode = (String)session.getAttribute("SCODE");
 		
-		// 페이징
+		
+		/*** 190925 재욱, begin 페이징 ***/
 		Map<String, Object> map = utilityService.utilityList(currentPage, contractShopCode, utilityKey, utilityValue, beginDate, endDate);
 		//System.out.println(map + " <-- map spendUtility UtilityController.java");
 		
@@ -70,105 +76,31 @@ public class UtilityController {
 			model.addAttribute("alert", "검색 결과가 없습니다");
 		}
 		
+		// 페이지 번호 model.addAttribute
 		model.addAttribute("startPageNum", startPageNum);
 		model.addAttribute("lastPageNum", lastPageNum);
 		model.addAttribute("currentPage", currentPage);
 		model.addAttribute("lastPage", lastPage);
 		model.addAttribute("utilityList", utilityList);
+		/*** 190925 재욱, end 페이징 ***/
 		
 
-		// 지출_공과금 등록 계정과목 select box 리스트
+		/*** 190925 재욱, 지출_공과금 등록 계정과목 select box 리스트 ***/ // 
 		List<SubjectDTO> subjectList = utilityService.subjectSelectBox();
 		model.addAttribute("subjectSelectBox", subjectList);
-
-		// 년도에 따른 월별 공과금 지출 금액 
-		List<UtilityPay> list = utilityService.utilityPayCheck(utilityYear, contractShopCode);
-		//System.out.println(list + " <-- list spendUtility UtilityController.java");
 		
+
+		/*** 190926 재욱, begin 년도에 따른 월별 공과금 지출 금액 차트 ***/
+		map.put("columnDate", "spend_utility_date");	// 조회할 날짜 db 컬럼
+		map.put("columnInt", "spend_utility_pay"); 		// 합산할 db 컬럼 
+		map.put("chartTable", "spend_utility");			// 조회할 db 테이블명
+		map.put("contractShopCode", contractShopCode);	// 검색 조건, contractShopCode
+		map.put("chartYear", utilityYear);				// 검색할 연도
 		
-		// 년도에 따른 월별 공과금 지출 금액 유무, 값이 없을 시 0원으로 보냄
-		for(int i=0; i<12; i++) {
-			if(i==0) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityJanuary", 0);
-				} else {
-					model.addAttribute("utilityJanuary", list.get(i).getSumUtility());
-				}
-			} else if(i==1) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityFebruary", 0);
-				} else {
-					model.addAttribute("utilityFebruary", list.get(i).getSumUtility());
-				}
-			} else if(i==2) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityMarch", 0);
-				} else {
-					model.addAttribute("utilityMarch", list.get(i).getSumUtility());
-				}
-			} else if(i==3) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityApril", 0);
-				} else {
-					model.addAttribute("utilityApril", list.get(i).getSumUtility());
-				}
-			} else if(i==4) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityMay", 0);
-				} else {
-					model.addAttribute("utilityMay", list.get(i).getSumUtility());
-				}
-			} else if(i==5) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityJune", 0);
-				} else {
-					model.addAttribute("utilityJune", list.get(i).getSumUtility());
-				}
-			} else if(i==6) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityJuly", 0);
-				} else {
-					model.addAttribute("utilityJuly", list.get(i).getSumUtility());
-				}
-			} else if(i==7) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityAugust", 0);
-				} else {
-					model.addAttribute("utilityAugust", list.get(i).getSumUtility());
-				}
-			} else if(i==8) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilitySeptember", 0);
-				} else {
-					model.addAttribute("utilitySeptember", list.get(i).getSumUtility());
-				}
-			} else if(i==9) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityOctober", 0);
-				} else {
-					model.addAttribute("utilityOctober", list.get(i).getSumUtility());
-				}
-			} else if(i==10) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityNovember", 0);
-				} else {
-					model.addAttribute("utilityNovember", list.get(i).getSumUtility());
-				}
-			} else if(i==11) {
-				if(list.get(i) == null) {
-					model.addAttribute("utilityDecember", 0);
-				} else {
-					model.addAttribute("utilityDecember", list.get(i).getSumUtility());
-				}
-			}
-
-		}
-
-		// 지출_공과금 차트 y축 여유 주기 위한 최대값
-		List<UtilityPay> payMax = utilityService.utilityPayMax(utilityYear, contractShopCode);
-		if(payMax.size() != 0) {
-			model.addAttribute("payMax", payMax.get(0).getSumUtility());
-		}
+		int[] chartValueArrays = mainService.chartValue(map);
+		System.out.println(Arrays.toString(chartValueArrays) + " <-- chartValueArrays spendUtility UtilityController.java");
+		/*** 190926 재욱, end 년도에 따른 월별 공과금 지출 금액 차트 ***/
+		
 		
 		return "spend/spendUtilityList";
 	}
