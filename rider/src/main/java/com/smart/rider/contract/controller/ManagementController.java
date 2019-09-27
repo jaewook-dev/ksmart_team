90c8fdbfa7c62ddec8d1765e8987097648bb2a85
Administrator <Administrator@smart20114>
2019-09-27 16:25:33 +0900

    석호 수정 : 날짜
    계약관리 날짜관리 더 효율적인 방법으로 코딩 및
    
    계약관리 등록시에 계약서 등록은 자동으로

--- rider/src/main/java/com/smart/rider/contract/controller/ManagementController.java
+++ rider/src/main/java/com/smart/rider/contract/controller/ManagementController.java
@@ -22,19 +22,14 @@ public class ManagementController {
 
 	@Autowired
 	private ManagementService managementService;
-	@Autowired
-	private ContractService contractService;
+
 	
 	
 	@GetMapping("/managementList")
-	public String managementList(Model model,HttpSession session) {
+	public String managementList(Model model) {
 		List<ContractManagementDTO> managementList = managementService.managementList();
 		
-		//contractList에 담겨있는 contractCode 중에  마지막에 등록된 코드 가져오기
-		List<ContractDTO> contractList =  contractService.contractList();
-		String getContractCode = contractList.get(contractList.size()-1).getContractCode();
-		System.out.println(getContractCode);
-		session.setAttribute("SCC",getContractCode);
+
 		//모델에 넣기 전에 managementList들어있는 값을 확인  
 		System.out.println("ContractManagementDTO" + managementList);
 		model.addAttribute("managementList", managementList);
@@ -42,20 +37,7 @@ public class ManagementController {
 		return "management/managementList";
 	}
 	
-	@GetMapping("/managementInsert")
-	public String managementInsert() {
-		
-		return "management/managementInsert";
-	}
-	
-	@PostMapping("/managementInsert")
-	public String managementInsert(ManagementDTO management,HttpSession session) {
-		
-		System.out.println(management.toString() + "<-- management.toString");
-		managementService.managementInsert(management, session);
-		
-		return "redirect:/managementList";
-	}
+
 	
 	@GetMapping("/managementUpdate")
 	public String managementUpdate(@RequestParam(value = "contractManagementCode") String managementCode, Model model) {
