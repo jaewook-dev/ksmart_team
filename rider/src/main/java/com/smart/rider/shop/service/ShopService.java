daa2684f46ce0f97ea492be92c5bf4f3d453d3a3
Administrator <Administrator@smart20114>
2019-09-30 09:15:20 +0900

    석호 수정:계약코드관리
    매장 등록시에 자동으로 계약코드 등록

--- rider/src/main/java/com/smart/rider/shop/service/ShopService.java
+++ rider/src/main/java/com/smart/rider/shop/service/ShopService.java
@@ -2,6 +2,8 @@ package com.smart.rider.shop.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +18,13 @@ public class ShopService {
 	@Autowired
 	private ShopMapper shopMapper;
 
+
 	public List<ShopDTO> shopList() {
 
 		return shopMapper.shopList();
 	}
 
-	public int shopInsert(ShopDTO shop) {
+	public int shopInsert(ShopDTO shop,HttpSession session,ShopRelationDTO relation) {
 		// 담겨있는 contractCode로 contractDTO조회
 		String contractCode = shop.getContractCode();
 		System.out.println(contractCode + "-->담겨 있는 값");
@@ -39,32 +42,34 @@ public class ShopService {
 
 		shop.setShopCode(shopCode);
 		shop.setMemberId(memberId);
-		 
+		
+		int result = 0;
+		result += shopMapper.shopInsert(shop);
+		
+		//shop 코드를 조회해서 가장 마지막 코드 값 가져오기
+		List<ShopDTO> shopList = shopList();
+		String getshopCode = shopList.get(shopList.size()-1).getShopCode();
+		String getmemberId = shopList.get(shopList.size()-1).getMemberId();
+		System.out.println(getshopCode);
+		session.setAttribute("SSHOPCODE",getshopCode);
+		
 
-		return shopMapper.shopInsert(shop);
-	}
-	public int relationInsert(ShopRelationDTO relation) {
-		// relation에 담겨 있는 값으로 id 조회하기
-		String shopCode = relation.getShopCode();
-		System.out.println(shopCode + "-->담겨 있는 값");
-		// 조회된 값에서 id값만 가져오기 및 확인작업
-		List<ShopDTO> List = shopMapper.getScode(shopCode);
-		System.out.println(List + "<--담긴 데이터");
-		String memberId = List.get(0).getMemberId();
-		System.out.println(memberId+"<--담긴 ID값");
-		 
 		//자동코드 증가
 		String relationCode = "SR"+ shopMapper.relationCodeMax();
 		if(relationCode.equals("SRnull")) { 
 			relationCode = "SR0001";
 		}
-
+		//relation 에 가장 마지막에 담은 Code 값과 id 값 담고 조합된 relationCode 생성
 		relation.setContractShopCode(relationCode);
-		relation.setMemberId(memberId);
-		 
+		relation.setMemberId(getmemberId);
+		relation.setShopCode(getshopCode);
 
-		return shopMapper.relationInsert(relation);
-		}
+		result += shopMapper.relationInsert(relation);
+
+		return result;
+	}
+	
+
 	public List<ShopRelationDTO> relationList(){
 		
 		return shopMapper.relationList();
