ce9d19ce78c39456f7fefa75b1b8cb3c9821124b
Administrator <Administrator@smart000>
2019-10-07 11:16:37 +0900

    재욱, 추가 : 지출_통합 차트
    spendTotal.html 지출_통합
    매입, 급여, 공과금/기타 지출 통합하여 차트로 보여줌

--- rider/src/main/java/com/smart/rider/spend/controller/SpendController.java
+++ rider/src/main/java/com/smart/rider/spend/controller/SpendController.java
@@ -1,6 +1,11 @@
 package com.smart.rider.spend.controller;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.smart.rider.main.service.MainService;
 import com.smart.rider.spend.dto.SpendAdminDTO;
 import com.smart.rider.spend.service.SpendService;
 
@@ -19,6 +26,70 @@ public class SpendController {
 	@Autowired
 	private SpendService spendService;
 
+	@Autowired
+	private MainService mainService;
+	
+	@GetMapping("/spendTotal")
+	public String spendTotal(@RequestParam(value = "totalYear", required = false, defaultValue = "2019") String totalYear
+							,Model model
+							,HttpSession session) {
+		
+		String contractShopCode = (String)session.getAttribute("SCODE");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		/** 191007 재욱, Read : 지출_통합 급여 월별 총 지출 금액 차트 **/
+		map.put("columnDate", "spend_salary_date");		// 조회할 날짜 db 컬럼
+		map.put("columnInt", "spend_salary_total"); 	// 합산할 db 컬럼 
+		map.put("chartTable", "spend_salary");			// 조회할 db 테이블명
+		map.put("contractShopCode", contractShopCode);	// 검색 조건, contractShopCode
+		map.put("chartYear", totalYear);				// 검색할 연도
+		
+		int[] salaryChartArrays = mainService.chartValue(map);
+		//System.out.println(Arrays.toString(salaryChartArrays) + " <-- salaryChartArrays spendTotal() SpendController.java");
+		
+		// to view model.addAttribute
+		for(int i=0; i<12; i++) { 
+			String salaryChart = "salary" + String.valueOf(i);
+			model.addAttribute(salaryChart, salaryChartArrays[i]);
+		}
+		
+		/** 191007 재욱, Read : 지출_통합 공과금/기타 월별 총 지출 금액 차트 **/
+		map.put("columnDate", "spend_utility_date");	// 조회할 날짜 db 컬럼
+		map.put("columnInt", "spend_utility_pay"); 	// 합산할 db 컬럼 
+		map.put("chartTable", "spend_utility");			// 조회할 db 테이블명
+		map.put("contractShopCode", contractShopCode);	// 검색 조건, contractShopCode
+		map.put("chartYear", totalYear);				// 검색할 연도
+		
+		int[] utilityChartArrays = mainService.chartValue(map);
+		//System.out.println(Arrays.toString(utilityChartArrays) + " <-- utilityChartArrays spendTotal SpendController.java");
+		
+		// to view model.addAttribute
+		for(int i=0; i<12; i++) { 
+			String utilityChart = "utility" + String.valueOf(i);
+			model.addAttribute(utilityChart, utilityChartArrays[i]);
+		}
+		
+		/** 191007 재욱, Read : 지출_통합 매입 월별 총 지출 금액 차트 **/
+		map.put("columnDate", "purchase_date");	// 조회할 날짜 db 컬럼
+		map.put("columnInt", "purchase_pay"); 	// 합산할 db 컬럼 
+		map.put("chartTable", "purchase");			// 조회할 db 테이블명
+		map.put("contractShopCode", contractShopCode);	// 검색 조건, contractShopCode
+		map.put("chartYear", totalYear);				// 검색할 연도
+		
+		int[] purchaseChartArrays = mainService.chartValue(map);
+		//System.out.println(Arrays.toString(purchaseChartArrays) + " <-- purchaseChartArrays spendTotal() SpendController.java");
+		
+		// to view model.addAttribute
+		for(int i=0; i<12; i++) { 
+			String purchaseChart = "purchase" + String.valueOf(i);
+			model.addAttribute(purchaseChart, purchaseChartArrays[i]);
+		}
+		
+		model.addAttribute("selectedYear", totalYear);
+		
+		return "spend/spendTotal";
+	}
 	
 	/**** 191007 재욱, 매장 지출 관리하기 화면이동 ****/
 	@PostMapping("/spendAdmin")
